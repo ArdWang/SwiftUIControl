a4cb986a6dd992ef821762f4e224e6841acfd157
ardwang <278161009@qq.com>
2021-01-19 15:05:20 +0800

    Initial Commit

---
+++ SwiftUIControl/SwiftUIControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIControlApp.swift
+//  SwiftUIControl
+//
+//  Created by RND on 2021/1/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
